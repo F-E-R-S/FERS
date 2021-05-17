165f28a31f30b18cb5f606c82670b8cecba851f2
Hogea <eduard.hogea00@e-uvt.ro>
2021-05-17 16:13:55 +0300

    Update 2

--- FERS/settings.py
+++ FERS/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -135,3 +135,5 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'ferssoftwares@gmail.com'
 EMAIL_HOST_PASSWORD ='djangoisgay'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
